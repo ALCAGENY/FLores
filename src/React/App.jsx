7a79dd5bdd81ced6ceaec8e7494bdb8d70bc2d77
ALCAGENY <rreshiran998@gmail.com>
2024-03-21 19:19:56 -0600

    Flores Nuevo 2

--- src/React/App.jsx
+++ src/React/App.jsx
@@ -8,7 +8,7 @@ const Inicio = () => {
     };
 
     const handleGoToFloresPage = () => {
-        window.location.href = 'https://taupe-pudding-c8338f.netlify.app/Flores'; // Aquí debes colocar la URL de la página a la que deseas navegar
+        window.location.href = 'http://localhost:5173/Flores'; // Aquí debes colocar la URL de la página a la que deseas navegar
     };
 
     return (
